4705dfb0c96bde12f4d94c3154b50ebcbae04c2c
John Lees-Miller <jdleesmiller@gmail.com>
2016-12-09 19:58:44 +0000

    Work on side camera training

--- model.py
+++ model.py
@@ -16,8 +16,42 @@ from sklearn.model_selection import train_test_split
 
 from common import *
 
+def make_features_and_labels_for_center_cam(
+        log, bottleneck_files, batch_indexes, label_column):
+    batch_features = np.array([
+        bottleneck_files[index]['center_image']
+        for index in batch_indexes
+    ])
+    batch_labels = log[label_column].values[batch_indexes]
+    return batch_features, batch_labels
+
+def make_features_and_labels_for_all_cams(
+        log, bottleneck_files, batch_indexes, label_column,
+        side_camera_bias):
+
+    center_features, center_labels = make_features_and_labels_for_center_cam(
+        log, bottleneck_files, batch_indexes)
+
+    left_features = np.array([
+        bottleneck_files[index]['left_image']
+        for index in batch_indexes
+    ])
+    left_labels = center_labels + side_camera_bias
+
+    right_features =  np.array([
+        bottleneck_files[index]['right_image']
+        for index in batch_indexes
+    ])
+    right_labels = center_labels - side_camera_bias
+
+    features = np.concatenate([left_features, center_features, right_features])
+    labels = np.concatenate([left_labels, center_labels, right_labels])
+
+    return features, labels
+
 def generate_data(log, indexes,
         label_column='smooth_steering_angle_1',
+        side_camera_bias=None,
         batch_size=32):
     # I think this uses weakrefs, so we can cache the files here.
     bottleneck_files = log['bottleneck_features'].map(
@@ -28,17 +62,17 @@ def generate_data(log, indexes,
         end = start + batch_size
         batch_indexes = indexes[start:end]
 
-        batch_features = np.array([
-            bottleneck_files[index]['center_image'] for index in batch_indexes
-        ])
-
-        batch_labels = log[label_column].values[batch_indexes]
+        if side_camera_bias is None:
+            yield make_features_and_labels_for_center_cam(
+                log, bottleneck_files, batch_indexes, label_column)
+        else:
+            yield make_features_and_labels_for_all_cams(
+                log, bottleneck_files, batch_indexes, label_column,
+                side_camera_bias)
 
         start = end
         if start >= len(indexes):
             start = 0
-
-        yield(batch_features, batch_labels)
 
 def split_training_set(log, test_size=0.2, random_state=42):
     return train_test_split(
